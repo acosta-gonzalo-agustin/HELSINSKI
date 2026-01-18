4eedf82eaf4958bbe83a74cbae930becea4b1253
acosta-gonzalo-agustin <acosta.gonzalo.agustin@gmail.com>
2026-01-18 19:56:19 -0300

    part one apps anecdotes, courseinfo and unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+
+const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick}  text = "good"/>
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} text = "statistics"/>
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => {
+
+  return (
+   
+      <button onClick={handleClick}>
+        {text}
+      </button>
+  )
+}
+
+
+const Statistics = ({ good, neutral, bad, text }) => {
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+        <h1>{text}</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  } else {
+  return (
+   <>
+      <h1>{text}</h1>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good + neutral + bad} />
+        <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+        <StatisticLine text="positive" value={(good / (good + neutral + bad)) * 100 + "%"} />    
+        </tbody>
+      </table>
+      
+    </>  
+  ) }
+}
+
+const StatisticLine = ({ text, value }) => (
+          <tr>
+            <td>{text}</td>
+            <td>{value} </td>
+          </tr>
+)
+
+
+
+
+
+
+export default App
